7156b2704bbb1cd7bda99a969e50bb25c4fe8baf
Júlia de Souza Gomes <julia.s.gomes@hotmail.com>
2024-10-14 12:42:53 -0400

    style: ajuste telas login e esqueceu a senha

--- src/app/components/adm/forgtpasswd/forgtpasswd.component.ts
+++ src/app/components/adm/forgtpasswd/forgtpasswd.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-forgtpasswd',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './forgtpasswd.component.html',
   styleUrl: './forgtpasswd.component.css'
 })
 export class ForgtpasswdComponent {
+  constructor(private fb: FormBuilder) { }
 
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email,
+        Validators.pattern('.*\\..*')
+      ])]
+    })
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
 }
