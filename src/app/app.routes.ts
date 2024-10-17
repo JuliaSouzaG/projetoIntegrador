0a76317e7d836d264501e1b74353a040237013b8
Júlia de Souza Gomes <julia.s.gomes@hotmail.com>
2024-10-17 13:34:33 -0400

    config: configura path vazia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ForgtpasswdComponent } from './components/adm/forgtpasswd/forgtpasswd.c
 import { CadstroadmComponent } from './components/adm/cadstroadm/cadstroadm.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'esqueceu-a-senha', component: ForgtpasswdComponent },
     { path: 'cadastro-administrador', component: CadstroadmComponent }
